cb57845d1fd762edce08f19e377d4261093b4cc2
Ederson Luís <edergp2009@gmail.com>
2020-05-19 22:09:33 -0300

    Exibindo erros no Input.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,10 +14,16 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  console.log(formRef);
+
   // Utiliza o useCallback quando vai criar uma funcao dentro de um componente
   const handleSubmit = useCallback(async (data: object) => {
     // console.log(data);
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -28,6 +37,11 @@ const SignUp: React.FC = () => {
       });
     } catch (err) {
       console.log(err);
+
+      const errors = getValidationErrors(err);
+
+      // Este ponto de interrogacao serve para ver se esta variavel exite pra depois chamar o setErrros.
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -39,7 +53,7 @@ const SignUp: React.FC = () => {
         <img src={logoImg} alt="GoBarber" />
 
         {/* <Form initialData={{ name: 'Diego' }} onSubmit={handleSubmit}> */}
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
