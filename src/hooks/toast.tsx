abe7e9d04994c060891549de22206fd424d1cf53
Ederson Luís <edergp2009@gmail.com>
2020-05-31 10:38:06 -0300

    Adicionando e removendo um toast

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,27 +1,54 @@
-import React, { createContext, useCallback, useContext } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { uuid } from 'uuidv4';
 
 import ToastContainer from '../components/ToastContainer';
 
+export interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string; // ponto ? é pq nao é obrigatorio colocar
+}
+
 interface ToastContextData {
-  addToast(): void;
-  removeToast(): void;
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
 }
 
 // Context
 const ToastContext = createContext<ToastContextData>({} as ToastContextData); // inicializar como um objeto vazio.
 
 const ToastProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast');
-  }, []);
-  const removeToast = useCallback(() => {
-    console.log('removeToast');
+  const [messages, setMessages] = useState<ToastMessage[]>([]); // Melhor lugar para armazenar qualquer informacao e no estadoda nossa aplicacao
+
+  // omit ele pega toas as propriedades da interface menos id
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      // console.log('addToast');
+      const id = uuid();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      // setMessages([...messages, toast]); // copio todos que tem la dentro. espeitando os conceitos de mutabilidade do react.
+      setMessages((oldMessages) => [...oldMessages, toast]);
+    },
+    [],
+  );
+  const removeToast = useCallback((id: string) => {
+    // console.log('removeToast');
+    // Pegando stado atual das messagens pelo state
+    setMessages((state) => state.filter((message) => message.id !== id));
   }, []);
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   );
 };
